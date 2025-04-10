d8b5f2eac1d78accbd45c51c4ad504ba9d6985a1
Raj-dev08 <lipikasen79@gmail.com>
2025-04-11 00:23:17 +0530

    Update user.model.js

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -22,7 +22,8 @@ const userSchema = new mongoose.Schema(
     },
     profilePic: {
         type: String,
-        default: "",
+        default: "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png
+          ",
     },
   },
   { timestamps: true }
